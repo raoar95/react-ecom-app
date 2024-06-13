48cb0e9170162df10fd8a282da65f50e6d7b9276
Arun Rao <yarun1995.ar@gmail.com>
2024-06-13 23:24:08 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import reactDom from "react-dom/client";
 import App from "./app/localShopApp";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AppContext } from "./state/context/AppContext";
 import { CartContext } from "./state/context/CartContext";
 
 const root = reactDom.createRoot(document.querySelector("#root"));
 root.render(
-  <BrowserRouter basename="/react-ecom-app">
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <AppContext>
       <CartContext>
         <App />
