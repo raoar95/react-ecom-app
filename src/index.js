3df6e3dee39eb6019b32f42d8df3c1f03ab3cdaf
Arun Rao <yarun1995.ar@gmail.com>
2024-06-13 23:43:21 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import reactDom from "react-dom/client";
 import App from "./app/localShopApp";
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+import { BrowserRouter } from "react-router-dom";
 import { AppContext } from "./state/context/AppContext";
 import { CartContext } from "./state/context/CartContext";
 
 const root = reactDom.createRoot(document.querySelector("#root"));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter >
     <AppContext>
       <CartContext>
         <App />
